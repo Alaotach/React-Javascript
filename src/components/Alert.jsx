09a4a1a7ccba0690e9cb37cd35cafe58e5ac4024
Alaotach <149323748+Alaotach@users.noreply.github.com>
2024-07-22 04:45:11 +0000

    build-1.

--- src/components/Alert.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Alert(props) {
-  if (props.alert.message != null){
-    return(
-        <div className="alert alert-success" role="alert">{props.alert.message}</div>
-    )
-}
-}
